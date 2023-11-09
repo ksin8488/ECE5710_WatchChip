785e52ef40dff76381ecf2bff76b8c54cb2f2517
nwestern93 <104111271+nwestern93@users.noreply.github.com>
2023-11-09 14:15:48 -0700

    Update digital_clock_time.v
    Added min/hour inc/dec

--- RTL/digital_clock/digital_clock_time.v
+++ RTL/digital_clock/digital_clock_time.v
@@ -3,10 +3,10 @@ module Digital_Clock(
     Clk_1sec,  //Clock with 1 Hz frequency
     reset,     //active high reset
     clock_enable, // used to determine mode output
-	min_inc,
-	min_dec,
-	hour_inc,
-	hour_dec,
+    min_inc,
+    min_dec,
+    hour_inc,
+    hour_dec,
     seconds,
     minutes,
     hours
@@ -16,31 +16,27 @@ module Digital_Clock(
     input Clk_1sec;  
     input reset;
     input clock_enable; // if value == 1 it's current time : if value == 0 it's stopwatch time
-	input min_inc;
-	input min_dec;
-	input hour_inc;
-	input hour_dec;
+    input min_inc;
+    input min_dec;
+    input hour_inc;
+    input hour_dec;
 //What are the Outputs? 
     output reg unsigned [5:0] seconds; // 6 bits needed for binary representation of 60 2^6 = 64
     output reg unsigned [5:0] minutes; // 6 bits needed for binary representation of 60 2^6 = 64
     output reg unsigned [5:0] hours; // 5 bits needed for binary representation of 24 2^5 = 32. *Changed to 6 bits for easy BCD
-//Internal variables.
-/*
-    reg [5:0] seconds; // 6 bits needed for binary representation of 60 2^6 = 64
-    reg [5:0] minutes; // 6 bits needed for binary representation of 60 2^6 = 64
-    reg [5:0] hours;  // 5 bits needed for binary representation of 24 2^5 = 32 *Changed to 6 bits for easy BCD
-	*/
+
 
    //Execute the always blocks when the Clock or reset inputs are 
     //changing from 0 to 1(positive edge of the signal)
-    always @(posedge(Clk_1sec) or posedge(reset)) //sensitivity list
+ always @(posedge(Clk_1sec) or posedge(reset) or posedge(min_inc) or posedge(min_dec) or 
+        posedge(hour_inc) or posedge(hour_dec))) //sensitivity list
     begin
         if(reset == 1'b1) begin  //check for active high reset.
            //reset the time.
             seconds <= 0;
             minutes <= 0;
             hours <= 0;  
-		end
+	end
         else if(Clk_1sec == 1'b1) begin  //at the beginning of each second
 			if(seconds == 59) begin
 				seconds <= 0;
@@ -56,11 +52,23 @@ module Digital_Clock(
 				else begin
 					minutes <= minutes + 1;
 				end
-			end
+			 end
 			else begin
 				seconds <= seconds + 1; //increment sec
 			end
+	  end
+	    if(min_inc == 1'b1) begin
+		    minutes <= minutes + 1;
 	    end
-	end
+	    if(min_dec == 1'b1) begin
+		    minutes <= minutes - 1;
+	    end
+	    if(hours_inc == 1'b1) begin
+		    minutes <= hours + 1;
+	    end
+	    if(hours_dec == 1'b1) begin
+		    minutes <= hours - 1;
+	    end
+    end
 
 endmodule
