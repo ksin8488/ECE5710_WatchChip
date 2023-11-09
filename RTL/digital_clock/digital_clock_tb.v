cf7f8716d8699d01599bcbd47209f8cf10d7c2c9
nwestern93 <104111271+nwestern93@users.noreply.github.com>
2023-11-09 14:18:06 -0700

    Update digital_clock_tb.v
    Added min/hour inc/dec

--- RTL/digital_clock/digital_clock_tb.v
+++ RTL/digital_clock/digital_clock_tb.v
@@ -1,14 +1,20 @@
+
 module tb_clock;
 
        // Inputs
     reg Clk_1sec;
     reg reset;
     reg clock_enable;
+    reg min_inc;
+    reg min_dec;
+    reg hour_inc;
+    reg hour_dec;
 
     // Outputs
     wire [5:0] seconds;
     wire [5:0] minutes;
     wire [5:0] hours;
+    wire mode; // may have to modify this
 
     // Instantiate the Unit Under Test (UUT)
     Digital_Clock uut (
@@ -17,16 +23,20 @@ module tb_clock;
         .seconds(seconds), 
         .minutes(minutes), 
         .hours(hours),
-        .clock_enable(clock_enable)
+        .clock_enable(clock_enable),
+	.min_inc(min_inc),
+	.min_dec(min_dec),
+	.hour_inc(hour_inc),
+	.hour_dec(hour_dec)
     );
     
     //Generating the Clock with `1 Hz frequency
     initial Clk_1sec = 0;
-    always #50 Clk_1sec = ~Clk_1sec;  //Every 0.5 sec toggle the clock.
+    always #500000000 Clk_1sec = ~Clk_1sec;  //Every 0.5 sec toggle the clock.
 
     initial begin
         reset = 1;
-        clock_enable = 1; // I just manually toggled this and verified with Modelsim that the enable affects the mode output
+        clock_enable = 0;
         // Wait 100 ns for global reset to finish
         #100; 
         reset = 0; 
