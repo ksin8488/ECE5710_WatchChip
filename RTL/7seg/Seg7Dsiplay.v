076978a85054937f07abbd88a3e47595be115c53
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2023-11-09 17:58:16 -0700

    Coded most of Seg7Display
    Most of the coding is done for Seg7Display BUT it has not been fully tested with a testbench just yet

--- RTL/7seg/Seg7Dsiplay.v
+++ RTL/7seg/Seg7Dsiplay.v
@@ -25,11 +25,31 @@ display_s1, display_m1, display_h1, display_s2, display_m2, display_h2, second);
 	output reg [6:0] display_h2;
 	output second; //MAY REMOVE! for blinking : ?
 	
-	reg [7:0] bcdSec;
-	reg [7:0] bcdMin;
-	reg [7:0] bcdHour;
+	//---Registers for internal use---
+	//BCD values stored
+	reg [7:0] bcdCurrSec;
+	reg [7:0] bcdCurrMin;
+	reg [7:0] bcdCurrHour;
+	reg [7:0] bcdStopSec;
+	reg [7:0] bcdStopMin;
+	reg [7:0] bcdStopHour;
+	//Seven-Segment values stored
+	//Current
+	reg[6:0] segCurrSec1;
+	reg[6:0]	segCurrSec2;
+	reg[6:0] segCurrMin1;
+	reg[6:0]	segCurrMin2;
+	reg[6:0]	segCurrHour1;
+	reg[6:0]	segCurrHour2;
+	//Stopwatch
+	reg[6:0] segStopSec1;
+	reg[6:0]	segStopSec2;
+	reg[6:0] segStopMin1;
+	reg[6:0]	segStopMin2;
+	reg[6:0]	segStopHour1;
+	reg[6:0]	segStopHour2;
 	
-	//Parameters for different State settings from watch_fsm module output
+	//---Parameters for different State settings from watch_fsm module output---
 	parameter S_STOPWATCH_HIDE_STOPPED	= 3'b000; //Stopwatch is stopped and hidden from view
 	parameter S_SET_H							= 3'b001; //Time-setting mode where user can set the hour
 	parameter S_SET_M							= 3'b010; //Time-setting mode where minute is set
@@ -37,89 +57,146 @@ display_s1, display_m1, display_h1, display_s2, display_m2, display_h2, second);
 	parameter S_STOPWATCH_SHOW_RUNNING	= 3'b100; //Show stopwatch, no blinking?
 	parameter S_STOPWATCH_RESET			= 3'b101; //Show stopwatch, time reset?
 	parameter S_STOPWATCH_HIDE_RUNNING	= 3'b110; //Show current time and stopwatch is still running in background
+		
+	//---Module Instantiations---
+	//BCD re-evaluates through always@(bin) statement in binToBCD module
+	//Current Face binToBCD modules
+	binToBCD bcdCurrS(
+	.bin (current_s),
+	.BCD (bcdCurrSec)
+	);
 	
-	//For whenever the state changes
-//	always@(state)
-//	begin
-//		
-//	end
+	binToBCD bcdCurrM(
+	.bin (current_m),
+	.BCD (bcdCurrMin)
+	);
+	
+	binToBCD bcdCurrH(
+	.bin (current_h),
+	.BCD (bcdCurrHour)
+	);
+	
+	//Stopwatch binToBCD modules
+	binToBCD bcdStopS(
+	.bin (stopwatch_s),
+	.BCD (bcdStopSec)
+	);
+	
+	binToBCD bcdStopM(
+	.bin (stopwatch_m),
+	.BCD (bcdStopMin)
+	);
+	
+	binToBCD bcdStopH(
+	.bin (stopwatch_h),
+	.BCD (bcdStopHour)
+	);
+	
+	//BCD to 7Seg binary modules
+	//Current Face modules
+	bcdToSeg7 bToS_CurrSec(
+	.bcd (bcdCurrSec),
+	.seg7OnesOut (segCurrSec1),
+	.seg7TensOut (segCurrSec2)
+	);
+	
+	bcdToSeg7 bToS_CurrMin(
+	.bcd (bcdCurrMin),
+	.seg7OnesOut (segCurrMin1),
+	.seg7TensOut (segCurrMin2)
+	);
 
-		//BCD re-evaluates through always@(bin) statement in binToBCD module
-		//Current Face binToBCD modules
-		binToBCD bcdCurrS(
-		.bin (current_s),
-		.BCD (bcdSec)
-		);
-		
-		binToBCD bcdCurrM(
-		.bin (current_m),
-		.BCD (bcdMin)
-		);
-		
-		binToBCD bcdCurrH(
-		.bin (current_h),
-		.BCD (bcdHour)
-		);
-		
-		//Stopwatch binToBCD modules
-		binToBCD bcdStopS(
-		.bin (stopwatch_s),
-		.BCD (bcdSec)
-		);
-		
-		binToBCD bcdStopM(
-		.bin (stopwatch_m),
-		.BCD (bcdMin)
-		);
-		
-		binToBCD bcdStopH(
-		.bin (stopwatch_h),
-		.BCD (bcdHour)
-		);
+	bcdToSeg7 bToS_CurrHour(
+	.bcd (bcdCurrHour),
+	.seg7OnesOut (segCurrHour1),
+	.seg7TensOut (segCurrHour2)
+	);
+
+	//Stopwatch
+	bcdToSeg7 bToS_StopSec(
+	.bcd (bcdCurrSec),
+	.seg7OnesOut (segStopSec1),
+	.seg7TensOut (segStopSec2)
+	);
+
+	bcdToSeg7 bToS_StopMin(
+	.bcd (bcdCurrSec),
+	.seg7OnesOut (segStopMin1),
+	.seg7TensOut (segStopMin2)
+	);
+
+	bcdToSeg7 bToS_StopHour(
+	.bcd (bcdCurrSec),
+	.seg7OnesOut (segStopHour1),
+	.seg7TensOut (segStopHour2)
+	);
 	
-	//NOT FINAL CODE! Just some basic outlines and plenty of spagetti...
 	always@(posedge clk)
 	begin
-		
-		//https://www.fpga4student.com/2017/09/seven-segment-led-display-controller-basys3-fpga.htm
-//		case(bcdSec[3:0])
-//			//seven-segment cathode display
-//			4'b0000 : begin display_s1 = 7'b0000001; end
-//			4'b0001 : begin display_s1 = 7'b0110000; end
-//			4'b0010 : begin display_s1 = 7'b1101101; end
-//			4'b0011 : begin display_s1 = 7'b1111001; end
-//			4'b0100 : begin display_s1 = 7'b0110011; end
-//			4'b0101 : begin display_s1 = 7'b1011011; end
-//			4'b0110 : begin display_s1 = 7'b1011111; end
-//			4'b0111 : begin display_s1 = 7'b1110000; end
-//			4'b1000 : begin display_s1 = 7'b1111111; end
-//			4'b1001 : begin display_s1 = 7'b1111011; end
-//			default : begin display_s1 = 7'b0000000; end
-//		endcase
-//		
-//		case(bcdSec[7:4])
-//			4'b0000 : begin display_s2 = 7'b0000001; end
-//			4'b0001 : begin display_s2 = 7'b0000001; end
-//			4'b0010 : begin display_s2 = 7'b0000001; end
-//			4'b0011 : begin display_s2 = 7'b0000001; end
-//			4'b0100 : begin display_s2 = 7'b0000001; end
-//			4'b0101 : begin display_s2 = 7'b0000001; end
-//			4'b0110 : begin display_s2 = 7'b0000001; end
-//			4'b0111 : begin display_s2 = 7'b0000001; end
-//			4'b1000 : begin display_s2 = 7'b0000001; end
-//			4'b1001 : begin display_s2 = 7'b0000001; end
-//			default : begin display_s2 = 7'b0000000; end
-//		endcase
+		case(state)
+			S_STOPWATCH_HIDE_STOPPED: begin //Shows the current time
+				display_s1 = segCurrSec1;
+				display_s2 = segCurrSec1;
+				display_m1 = segCurrMin1;
+				display_m2 = segCurrMin2;
+				display_h1 = segCurrHour1;
+				display_h2 = segCurrHour2;
+			end
+			
+			S_SET_H: begin	//Shows stopwatch time
+				display_s1 = segCurrSec1;
+				display_s2 = segCurrSec1;
+				display_m1 = segCurrMin1;
+				display_m2 = segCurrMin2;
+				display_h1 = segCurrHour1;
+				display_h2 = segCurrHour2;
+			end
+			
+			S_STOPWATCH_SHOW_STOPPED: begin //Shows stopwatch time
+				display_s1 = segStopSec1;
+				display_s2 = segStopSec1;
+				display_m1 = segStopMin1;
+				display_m2 = segStopMin2;
+				display_h1 = segStopHour1;
+				display_h2 = segStopHour2;
+			end
+			
+			S_STOPWATCH_SHOW_RUNNING: begin //shows stopwatch time
+				display_s1 = segStopSec1;
+				display_s2 = segStopSec1;
+				display_m1 = segStopMin1;
+				display_m2 = segStopMin2;
+				display_h1 = segStopHour1;
+				display_h2 = segStopHour2;
+			end
+			
+			S_STOPWATCH_RESET: begin //shows stopwatch time
+				display_s1 = segStopSec1;
+				display_s2 = segStopSec1;
+				display_m1 = segStopMin1;
+				display_m2 = segStopMin2;
+				display_h1 = segStopHour1;
+				display_h2 = segStopHour2;
+			end
+			
+			S_STOPWATCH_HIDE_RUNNING: begin //shows current time
+				display_s1 = segCurrSec1;
+				display_s2 = segCurrSec1;
+				display_m1 = segCurrMin1;
+				display_m2 = segCurrMin2;
+				display_h1 = segCurrHour1;
+				display_h2 = segCurrHour2;
+			end
+			
+			default: begin //All outputs set to 0 for default case
+				display_s1 = 0;
+				display_s2 = 0;
+				display_m1 = 0;
+				display_m2 = 0;
+				display_h1 = 0;
+				display_h2 = 0;
+			end
+		endcase
 			
 	end
 endmodule
-
-
-//Here's an idea of how I would do things.
-//S_STOPWATCH_HIDE_STOPPED: in this state, the stopwatch is stopped and hidden. So you'll need to display the current time on the watch face, as normal - set digits to current_s/m/h.
-//S_SET_H: Ideally we'll need some way to communicate to the user that it's in time-setting mode. Still set digits to current_s/m/h, but also think about blinking the HH digits. You can do this with seconds_clk, just alternate between the digit and 7'b0 on each posedge seconds_clk. Let me know if you have questions about how to implement that, ChatGPT may be able to get you started too but it's been down for me lately.
-//S_SET_M: same idea different digits
-//S_STOPWATCH_SHOW_STOPPED: show stopwatch in this state, maybe blink it here too so the user knows it's stopped?
-//S_STOPWATCH_SHOW_RUNNING: show stopwatch, no blink
-//S_STOPWATCH_RESET: show stopwatch
-//S_STOPWATCH_HIDE_RUNNING: show current time
